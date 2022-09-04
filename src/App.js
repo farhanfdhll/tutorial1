7845918bae637f7412bcc8f38575f7692289ca1e
farhanfdhll <54814543+farhanfdhll@users.noreply.github.com>
2022-09-04 21:53:59 +0700

    mantap

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Farhan";
+  const handleRandomName =() =>{
+    const names = ['Bob', 'Kevin', 'Farhan', 'Ronny'];
+    const int = Math.floor(Math.random()*3);
+
+    return names[int]
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+        <p>My name is <strong>{name}</strong></p>
+        {/* <p>{[1, 2, 4]}</p> */}
+        {/* <p>{"This is a Javascript expression"}</p> */}
+        {/* <p>{{React cannot display an Object}}</p> */}
+        {/* <p>{true == false}</p> React cannot display boolean value*/}
+
+        <p>Today's random name is <strong>{handleRandomName()}</strong></p>
       </header>
+      
     </div>
   );
 }

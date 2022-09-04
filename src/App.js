0c2a2003e9bd7964061198b6fe6869a003edf0ca
farhanfdhll <54814543+farhanfdhll@users.noreply.github.com>
2022-09-04 22:05:50 +0700

    Komponen footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
 
 function App() {
-
-  const name = "Farhan";
-  const handleRandomName =() =>{
-    const names = ['Bob', 'Kevin', 'Farhan', 'Ronny'];
-    const int = Math.floor(Math.random()*3);
-
-    return names[int]
-  }
 
   return (
     <div className="App">
       <Header></Header>
-      
+      <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Content = () => {
+    const name = "Farhan";
+    const handleRandomName =() =>{
+        const names = ['Bob', 'Kevin', 'Farhan', 'Ronny'];
+        const int = Math.floor(Math.random()*3);
+
+        return names[int]
+    }
   return (
+    
     <main>
-        
+        <p>My name is <strong>{name}</strong></p>
+        {/* <p>{[1, 2, 4]}</p> */}
+        {/* <p>{"This is a Javascript expression"}</p> */}
+        {/* <p>{{React cannot display an Object}}</p> */}
+        {/* <p>{true == false}</p> React cannot display boolean value*/}
+
+        <p>Today's random name is <strong>{handleRandomName()}</strong></p>
     </main>
   )
 }

ac132e81fe721ded97464d66270e669a4b4472fd
farhanfdhll <54814543+farhanfdhll@users.noreply.github.com>
2022-09-05 06:32:13 +0700

    jsx

--- src/Content.js
+++ src/Content.js
@@ -1,55 +1,55 @@
 import React from 'react'
 import { useState } from 'react';
+import { BsTrashFill } from 'react-icons/bs';
 
 const Content = () => {
-    const [name, setName] = useState('Farhan');
-    // const name = "Farhan"; // this is vanilla js use useState instead ^^^
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+    const handleCheck = (id) =>{
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
+        setItems(listItems)
+        localStorage.setItem('shopping list', JSON.stringify(listItems))
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems)
+        localStorage.setItem('shopping list', JSON.stringify(listItems))
+    }
+    
 
     const [count, setCount] = useState(0);
-    const incrementCount = () =>{
-        setCount(count+1)
-    }
-    
-    const handleRandomName =() =>{
-        const names = ['Bob', 'Kevin', 'Rully', 'Ronny'];
-        const int = Math.floor(Math.random()*4);
+    // Check github commit for previously deleted code
 
-        setName(names[int]);
-        // return names[int]
-    }
-
-    const handleClick = () => {
-        console.log('You Clicked a Button')
-    }
-
-    const handleClick2 = (name) => {
-        console.log(`${name} was cliked` )
-    }
-
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
-
-    const handleClick4 = () => {
-        incrementCount()
-        console.log(count)
-    }
   return (
-    
     <main>
-        <p>Hello user <strong>{name}</strong>!</p>
-        {/* <p>{[1, 2, 4]}</p> */}
-        {/* <p>{"This is a Javascript expression"}</p> */}
-        {/* <p>{{React cannot display an Object}}</p> */}
-        {/* <p>{true == false}</p> React cannot display boolean value*/}
-
-        <button onClick={handleClick}> Click Me! </button>
-        <button onClick={() => handleClick2('Dave')}> Click Me Again! </button>
-        <button onClick={(e) => handleClick3(e)}> Click Me Event! </button>
+        {items.length ? (
+            <ul>
+                { items.map((item) => (
+                    <li className='item' key={item.id}>
+                        <input type="checkbox" checked={item.checked} onChange = {() => handleCheck(item.id)}/>
+                        <label style={(item.checked) ? {textDecoration: 'line-through'} : null} onDoubleClick={() => handleCheck(item.id)} htmlFor="">{item.item}</label>
+                        <BsTrashFill role='button' onClick={ () => handleDelete(item.id)} tabIndex='0'></BsTrashFill> 
+                    </li>
+                )) }
+            </ul>
+        ) : (<p style={{marginTop: '2rem'}}>Your list is <strong>empty</strong></p>) }
         
-        <button onClick={handleRandomName}> Change Name! </button>
-        
-        <button onClick={handleClick4}> Print Counter </button>
     </main>
   )
 }

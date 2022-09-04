0cae6671f83ffd1f1ba6f408dddd0544d2dbe757
farhanfdhll <54814543+farhanfdhll@users.noreply.github.com>
2022-09-04 22:27:32 +0700

    Events

--- src/Content.js
+++ src/Content.js
@@ -3,21 +3,36 @@ import React from 'react'
 const Content = () => {
     const name = "Farhan";
     const handleRandomName =() =>{
-        const names = ['Bob', 'Kevin', 'Farhan', 'Ronny'];
-        const int = Math.floor(Math.random()*3);
+        const names = ['Bob', 'Kevin', 'Rully', 'Ronny'];
+        const int = Math.floor(Math.random()*4);
 
         return names[int]
+    }
+
+    const handleClick = () => {
+        console.log('You Clicked a Button')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was cliked` )
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
     }
   return (
     
     <main>
-        <p>My name is <strong>{name}</strong></p>
+        <p>Hello user <strong>{name}</strong>!</p>
         {/* <p>{[1, 2, 4]}</p> */}
         {/* <p>{"This is a Javascript expression"}</p> */}
         {/* <p>{{React cannot display an Object}}</p> */}
         {/* <p>{true == false}</p> React cannot display boolean value*/}
 
-        <p>Today's random name is <strong>{handleRandomName()}</strong></p>
+        <p>Today's random companion name is <strong>{handleRandomName()}</strong></p>
+        <button onClick={handleClick}> Click Me! </button>
+        <button onClick={() => handleClick2('Dave')}> Click Me Again! </button>
+        <button onClick={(e) => handleClick3(e)}> Click Me Event! </button>
     </main>
   )
 }

b473e4b067a5c8001c4864d2ac1084804975a01d
farhanfdhll <54814543+farhanfdhll@users.noreply.github.com>
2022-09-09 20:50:55 +0700

    Controlled Component Inputs

--- src/LineItem.js
+++ src/LineItem.js
@@ -3,7 +3,7 @@ import { BsTrashFill } from 'react-icons/bs';
 
 const LineItem = ({handleCheck, handleDelete, item}) => {
   return (
-    <li className='item' key={item.id}>
+    <li className='item'>
         <input type="checkbox" checked={item.checked} onChange = {() => handleCheck(item.id)}/>
         <label style={(item.checked) ? {textDecoration: 'line-through'} : null} onDoubleClick={() => handleCheck(item.id)} htmlFor="">{item.item}</label>
         <BsTrashFill role='button' onClick={ () => handleDelete(item.id)} tabIndex='0' aria-label={`Delete ${item.item}`}></BsTrashFill> 
